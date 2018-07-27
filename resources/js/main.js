c19f5058884cc18493c6e379aa31300da4434d19
Janine Villareal <janine.villareal@gmail.com>
2018-07-27 05:40:36 -0600

    Started about page and minor fixes

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
 
+  /* Fade between pages*/
+  $('body').css('display', 'none');
+  $('body').fadeIn(1500);
+
+  /*Full page scroll plugin*/
   $('#fullpage').fullpage({
   scrollBar: true,
   navigation: true
